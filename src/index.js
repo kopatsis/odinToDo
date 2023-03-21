ceb80df03fabc7ad9d97e45462ee029df91957fb
Demetrios <jkopatsis@gmail.com>
2023-03-21 11:33:21 -0700

    task display success

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ close.addEventListener('click', () => {
     formOuter.classList.remove('open');
 });
 window.addEventListener('keyup', function esc(e){
-    if (e.key == 'Escape') formOuter.classList.remove('open');
+    if (e.key == 'Escape' && formOuter.classList.contains('open')) formOuter.classList.remove('open');
+    if (e.key == 'Escape' && dispOuter.classList.contains('open')) dispOuter.classList.remove('open');
 });
 window.addEventListener('click', function escape(e){
     if (e.target.classList.contains('formOuter') && e.target.classList.contains('open')) formOuter.classList.remove('open');
+    if (e.target.classList.contains('dispOuter') && e.target.classList.contains('open')) dispOuter.classList.remove('open');   
 });
 
 const taskCollection = [];
@@ -26,14 +28,16 @@ const description = document.querySelector('#description');
 const date = document.querySelector('#date');
 const form = document.querySelector('#createform');
 
-form.addEventListener('submit', function todoCreate(e){
+form.addEventListener('submit', todoCreate);
+
+function todoCreate(e){
     e.preventDefault();
     let formData = new FormData(form);
     form.reset();
     let vals = []
     for (const pair of formData.entries()) {
         if (pair[1] == ''){
-            vals.push(null);
+            vals.push("N/A");
         } else{
             vals.push(pair[1]);
         }
@@ -50,13 +54,13 @@ form.addEventListener('submit', function todoCreate(e){
     tempdisp.classList.add('todoDisp');
     tempdisp.classList.add(`${taskCollection.length-1}`);
     tempdisp.textContent = "View Details";
-    // Add onclick here
+    tempdisp.onclick = displayTask;
 
     const tempcomp = document.createElement('button');
     tempcomp.classList.add('todoComp');
     tempcomp.classList.add(`${taskCollection.length-1}`);
     tempcomp.textContent = "Mark Complete";
-    // Add onclick here
+    tempcomp.onclick = completeTask;
 
     const temphouse = document.createElement('div');
     temphouse.classList.add('todoHouse');
@@ -67,8 +71,29 @@ form.addEventListener('submit', function todoCreate(e){
 
     tdsect.appendChild(temphouse);
     formOuter.classList.remove('open');
-});
+}
 
+const dispInner = document.querySelector('.dispInner');
+const dispOuter = document.querySelector('.dispOuter');
+const dispClose = document.querySelector('.dispClose');
+dispClose.addEventListener('click', () =>{
+    dispOuter.classList.remove('open');
+})
+function displayTask(e){
+    dispOuter.classList.add('open');
+    dispInner.innerHTML = `
+    <button class="dispClose">× Close</button>
+    <div class="dispname">Task Name: ${taskCollection[parseInt(e.target.classList[1])].name}</div>
+    <div class="dispcreator">Task Creator: ${taskCollection[parseInt(e.target.classList[1])].creator}</div>
+    <div class="dispdesc">Description: ${taskCollection[parseInt(e.target.classList[1])].description} </div>
+    <div class="dispdate">Date Created: ${taskCollection[parseInt(e.target.classList[1])].date}</div>
+    `;
+}
+
+function completeTask(e){
+    console.log('comp');
+    console.log(e.target.classList);
+}
 
 function todoFactory(name, creator, description, date){
     return {
